d095b841fcdb39509012c91ae335c278250eb847
Martin Accou <martin.accou@icloud.com>
2025-09-09 09:58:38 +0200

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,13 @@
 // best implementation ever
 fn main() {
     // first comment
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+fn print_goodbye() {
+    println!("Goodbye, world!");
 }
